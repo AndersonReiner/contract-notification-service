c051a076a4243caca8da46d99260380b02a60cbe
AndersonReiner <reiner.academico@gmail.com>
2024-07-28 17:02:58 -0400

    firt commit

---
+++ src/main/java/com/anderson/contract/notification/service/ContractNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.anderson.contract.notification.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anderson/contract/notification/service/ContractNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anderson.contract.notification.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
